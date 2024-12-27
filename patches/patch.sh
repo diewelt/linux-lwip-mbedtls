2ad84722b5e27d5b05eb2c7cd923c623e1e06ab4
diewelt <kyle.s.shim@gmail.com>
2024-12-28 00:49:11 +1100

    mbedtls: add patches to disable platform network api

--- patches/patch.sh
+++ patches/patch.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+if [ ! -e mbedtls/framework/CMakeLists.txt ]; then
+    cd mbedtls
+    git submodule update --init
+    cd ..
+fi
+
 cd lwip
 if [ ! -e patched ]; then
     git clean -xdf
     git checkout .
     git apply ../patches/lwip-0001-cmakelists.patch
 fi
+cd ..
+
+cd mbedtls
+if [ ! -e patched ]; then
+    git clean -xdf
+    git checkout .
+    git apply ../patches/mbedtls-1001-disable-platform-network-api.patch
+fi
+cd ..
